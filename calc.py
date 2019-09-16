69cf3733065dfb034e7085958bd68e51e64b41da
Pranav Modi <pranav.modi@in.ibm.com>
2019-09-16 07:05:36 +0000

    Initial Commit

---
+++ calc.py
@@ -0,0 +1,17 @@
+def add(x,y):
+*** Add Function ***
+return x+y
+
+def subtract(x,y):
+    *** Subtract ***
+    return x-y
+
+def multiply(x,y):
+    *** Multiply ***
+    return x*y
+
+def divide(x,y):
+    *** Divide ***
+    return x/y
+
+
